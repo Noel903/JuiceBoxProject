642f2b0ab9ea8e64918f30009d1f1040cd274196
Noel903 <noelmoreno445@gmail.com>
2022-06-02 19:28:40 -0500

    corrected seed.js and index.js in db

--- db/seed.js
+++ db/seed.js
@@ -1,39 +1,66 @@
 // inside db/seed.js
 
 // grab our client with destructuring from the export in index.js
-const { client } = require('./index');
+const { client, getAllUsers } = require('./index');
 
 async function testDB() {
   try {
     // connect the client to the database, finally
     client.connect();
 
+    const users = await getAllUsers();
+    console.log(users);
     // queries are promises, so we can await them
-    const result = await client.query(`SELECT * FROM users;`);
+    // const result = await client.query(`SELECT * FROM users;`);
+    // console.log(result);
+
+    // const { rows } = await client.query(`SELECT * FROM users;`);
+    // console.log(rows);
 
     // for now, logging is a fine way to see what's up
-    console.log(result);
   } catch (error) {
     console.error(error);
   } finally {
     // it's important to close out the client connection
     client.end();
   }
-};
+}
 
 testDB();
 
-async function testDB() {
+// this function should call a query which drops all tables from our database
+async function dropTables() {
+    try {
+      await client.query(`
+  
+      `);
+    } catch (error) {
+      throw error; // we pass the error up to the function that calls dropTables
+    }
+  }
+  
+  // this function should call a query which creates all tables for our database 
+  async function createTables() {
+    try {
+      await client.query(`
+  
+      `);
+    } catch (error) {
+      throw error; // we pass the error up to the function that calls createTables
+    }
+  }
+  
+  async function rebuildDB() {
     try {
       client.connect();
   
-      const { rows } = await client.query(`SELECT * FROM users;`);
-      console.log(rows);
+      await dropTables();
+      await createTables();
     } catch (error) {
       console.error(error);
     } finally {
-        
       client.end();
     }
-  };
+  }
   
+  rebuildDB();
